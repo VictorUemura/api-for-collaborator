1382a140ef728be3933d2072e89d6fbf70ce7a87
VictorUemura <victoruemura04@hotmail.com>
2024-03-18 07:51:34 -0300

    feat: depuracao + criacao de modelo Colaborador

---
+++ Models/Colaborador.cs
@@ -0,0 +1,9 @@
+public class Colaborador
+{
+    public int codigo {get;}
+    public string? nome { get; set; }
+    public string? endereco { get; set;}
+    public string? telefone {get;set;}
+    public string? cargo {get;set;}
+    public double salario {get;set;}
+}
