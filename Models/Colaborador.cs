f659945aad953d5e7673bad7efa17678114aa561
VictorUemura <victoruemura04@hotmail.com>
2024-03-18 07:54:49 -0300

    Modelo de classe colaborador apagado

--- Models/Colaborador.cs
+++
@@ -1,9 +0,0 @@
-public class Colaborador
-{
-    public int codigo {get;}
-    public string? nome { get; set; }
-    public string? endereco { get; set;}
-    public string? telefone {get;set;}
-    public string? cargo {get;set;}
-    public double salario {get;set;}
-}
