33f295863d39c58b3072ef0aca062377553d0cbb
VictorUemura <victoruemura04@hotmail.com>
2024-03-18 21:28:36 -0300

    compilacao e alteracao na estrutura do namespace

--- Controllers/UsuarioController.cs
+++ Controllers/UsuarioController.cs
@@ -1,52 +1,85 @@
 using Api_test.Models;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
-namespace Api.Controllers;
-
-[ApiController]
-[Route("[controller]")]
-public class UsuarioController : ControllerBase
+namespace Api.Controllers
 {
-    private readonly UsuarioContext _context;
-    public UsuarioController(UsuarioContext context)
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
     {
-        _context = context;
-    }
-
-    [HttpDelete]
-    public async Task<IActionResult> DeleteUsuarioModel(int id)
-    {
-        var usuario = await _context.UsuarioItems.FindAsync(id);
-        if (usuario == null)
+        private readonly UsuarioContext _context;
+        public UsuarioController(UsuarioContext context)
         {
-            return NotFound();
+            _context = context;
+        }
+        [HttpDelete]
+        public async Task<IActionResult> DeleteUsuarioModel(int id)
+        {
+            var usuario = await _context.UsuarioItems.FindAsync(id);
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+
+            _context.UsuarioItems.Remove(usuario);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<UsuarioModel>> GetUsuarioModel(int id)
+        {
+            var usuarioModel = await _context.UsuarioItems.FindAsync(id);
+
+            if (usuarioModel == null)
+            {
+                return NotFound();
+            }
+
+            return usuarioModel;
+        }
+        [HttpPost]
+        public async Task<ActionResult<UsuarioModel>> PostUsuarioModel(UsuarioModel UsuarioModel)
+        {
+            _context.UsuarioItems.Add(UsuarioModel);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetUsuarioModel), new { id = UsuarioModel.Id }, UsuarioModel);
         }
 
-        _context.UsuarioItems.Remove(usuario);
-        await _context.SaveChangesAsync();
-
-        return NoContent();
-    }
-
-    [HttpGet("{id}")]
-    public async Task<ActionResult<UsuarioModel>> GetUsuarioModel(int id)
-    {
-        var usuarioModel = await _context.UsuarioItems.FindAsync(id);
-
-        if (usuarioModel == null)
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutUsuarioModel(long id, UsuarioModel usuario)
         {
-            return NotFound();
+            if (id != usuario.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(usuario).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UsuarioModelExist(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return NoContent();
         }
 
-        return usuarioModel;
+        private bool UsuarioModelExist(long id)
+        {
+            return _context.UsuarioItems.Any(e => e.Id == id);
+        }
     }
 
-    [HttpPost]
-    public async Task<ActionResult<UsuarioModel>> PostUsuarioModel(UsuarioModel UsuarioModel)
-    {
-        _context.UsuarioItems.Add(UsuarioModel);
-        await _context.SaveChangesAsync();
-
-        return CreatedAtAction(nameof(GetUsuarioModel), new { id = UsuarioModel.Id }, UsuarioModel);
-    }
 }
--- obj/Debug/net8.0/Api.AssemblyInfo.cs
+++ obj/Debug/net8.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb328e05020c30a0628c725fd3306fc86d9e3794")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+128c4d6a170e0be4e15bd16f88276731c427f601")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
