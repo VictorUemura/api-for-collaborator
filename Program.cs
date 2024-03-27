965c6428ae14989df882b1e6a4865c60f5f76fe3
JeffBianchi <jecom2013@gmail.com>
2024-03-27 14:52:40 -0300

    Integração DB

--- Program.cs
+++ Program.cs
@@ -3,15 +3,19 @@ using JsonPatchSample;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ColaboradorContext>(options =>
+    options.UseMySql(
+        "server=localhost;initial catalog=ColaboradorAPI;uid=root;pwd=Tb@159753",
+        Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.36-mysql")));
+
 builder.Services.AddControllers(options =>
 {
     options.InputFormatters.Insert(0, MyJPIF.GetJsonPatchInputFormatter());
 });
-builder.Services.AddControllers()
-    .AddNewtonsoftJson();
+
+builder.Services.AddControllers().AddNewtonsoftJson();
 builder.Services.AddControllers();
-builder.Services.AddDbContext<ColaboradorContext>(opt =>
-    opt.UseInMemoryDatabase("ColaboradorList"));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
