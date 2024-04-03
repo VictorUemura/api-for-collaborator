f042a273aa23758aa549f9f5cfecfee52e7342b5
JeffBianchi <jecom2013@gmail.com>
2024-04-03 07:44:48 -0300

    Fluent Validation / Service Response

--- Controllers/ColaboradorController.cs
+++ Controllers/ColaboradorController.cs
@@ -1,9 +1,9 @@
-using Api_test.Models;
 using Api_test.Repositories;
-using Microsoft.AspNetCore.Http.HttpResults;
+using Api_test.Models;
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using FluentValidation;
 
 namespace Api_test.Controllers
 {
@@ -12,74 +12,12 @@ namespace Api_test.Controllers
     public class ColaboradorController : ControllerBase
     {
         private readonly ColaboradorContext _context;
-        public ColaboradorController(ColaboradorContext context)
+        private readonly IValidator<ColaboradorModel> _validator;
+
+        public ColaboradorController(ColaboradorContext context, IValidator<ColaboradorModel> validator)
         {
             _context = context;
-        }
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<ColaboradorModel>>> GetColaboradorModel()
-        {
-            return await _context.ColaboradorItems.ToListAsync();
-        }
-        [HttpDelete]
-        public async Task<IActionResult> DeleteColaboradorModel(int id)
-        {
-            var colaboradorModel = await _context.ColaboradorItems.FindAsync(id);
-            if (colaboradorModel == null)
-            {
-                return NotFound();
-            }
-
-            _context.ColaboradorItems.Remove(colaboradorModel);
-            await _context.SaveChangesAsync();
-            return NoContent();
-        }
-        [HttpGet("{id}")]
-        public async Task<ActionResult<ColaboradorModel>> GetColaboradorModel(int id)
-        {
-            var colaboradorModel = await _context.ColaboradorItems.FindAsync(id);
-
-            if (colaboradorModel == null)
-            {
-                return NotFound();
-            }
-
-            return colaboradorModel;
-        }
-        [HttpPost]
-        public async Task<ActionResult<ColaboradorModel>> PostColaboradorModel(ColaboradorModel ColaboradorModel)
-        {
-            _context.ColaboradorItems.Add(ColaboradorModel);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction(nameof(GetColaboradorModel), new { id = ColaboradorModel.Id }, ColaboradorModel);
-        }
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutColaboradorModel(long id, ColaboradorModel colaboradorModel)
-        {
-            if (id != colaboradorModel.Id)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(colaboradorModel).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!ColaboradorModelExist(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-            return NoContent();
+            _validator = validator;
         }
 
         private bool ColaboradorModelExist(long id)
@@ -87,49 +25,166 @@ namespace Api_test.Controllers
             return _context.ColaboradorItems.Any(e => e.Id == id);
         }
 
-        [HttpPatch("{id}")]
-        public async Task<IActionResult> PatchColaboradorModel(int id, JsonPatchDocument<ColaboradorModel> patchDocument)
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ColaboradorModel>>> GetColaboradorModel()
         {
-            if (patchDocument == null)
+            try
             {
-                return BadRequest("O documento de patch não pode ser nulo.");
+                var colaboradores = await _context.ColaboradorItems.ToListAsync();
+                return Ok(new ServiceResponse<IEnumerable<ColaboradorModel>> { Dados = colaboradores, Mensagem = "Colaboradores recuperados com sucesso." });
             }
-
-            var colaboradorModel = await _context.ColaboradorItems.FindAsync(id);
-
-            if (colaboradorModel == null)
+            catch (Exception ex)
             {
-                return NotFound();
+                return StatusCode(500, new ServiceResponse<IEnumerable<ColaboradorModel>> { Mensagem = $"Ocorreu um erro ao recuperar os colaboradores: {ex.Message}", Sucesso = false });
             }
+        }
 
-            patchDocument.ApplyTo(colaboradorModel, ModelState);
-
-            if (!ModelState.IsValid)
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteColaboradorModel(int id)
+        {
+            try
             {
-                return BadRequest(ModelState);
-            }
+                var colaboradorModel = await _context.ColaboradorItems.FindAsync(id);
+                if (colaboradorModel == null)
+                {
+                    return NotFound(new ServiceResponse<object> { Mensagem = "Colaborador não encontrado.", Sucesso = false });
+                }
 
-            _context.Entry(colaboradorModel).State = EntityState.Modified;
+                _context.ColaboradorItems.Remove(colaboradorModel);
+                await _context.SaveChangesAsync();
+                return Ok(new ServiceResponse<object> { Mensagem = "Colaborador excluído com sucesso." });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new ServiceResponse<object> { Mensagem = $"Ocorreu um erro ao excluir o colaborador: {ex.Message}", Sucesso = false });
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ColaboradorModel>> GetColaboradorModel(int id)
+        {
+            try
+            {
+                var colaboradorModel = await _context.ColaboradorItems.FindAsync(id);
+
+                if (colaboradorModel == null)
+                {
+                    return NotFound(new ServiceResponse<ColaboradorModel> { Mensagem = "Colaborador não encontrado.", Sucesso = false });
+                }
+
+                return Ok(new ServiceResponse<ColaboradorModel> { Dados = colaboradorModel, Mensagem = "Colaborador recuperado com sucesso." });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new ServiceResponse<ColaboradorModel> { Mensagem = $"Ocorreu um erro ao recuperar o colaborador: {ex.Message}", Sucesso = false });
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ColaboradorModel>> PostColaboradorModel(ColaboradorModel colaboradorModel)
+        {
+            var validationResult = _validator.Validate(colaboradorModel);
+            if (!validationResult.IsValid)
+            {
+                return BadRequest(new ServiceResponse<ColaboradorModel> { Mensagem = "Erro de validação", Sucesso = false });
+            }
 
             try
             {
+                _context.ColaboradorItems.Add(colaboradorModel);
                 await _context.SaveChangesAsync();
+
+                return CreatedAtAction(nameof(GetColaboradorModel), new { id = colaboradorModel.Id }, new ServiceResponse<ColaboradorModel> { Dados = colaboradorModel, Mensagem = "Colaborador criado com sucesso." });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new ServiceResponse<ColaboradorModel> { Mensagem = $"Ocorreu um erro ao criar o colaborador: {ex.Message}", Sucesso = false });
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutColaboradorModel(long id, ColaboradorModel colaboradorModel)
+        {
+            try
+            {
+                if (id != colaboradorModel.Id)
+                {
+                    return BadRequest(new ServiceResponse<ColaboradorModel> { Mensagem = "ID do colaborador na URL não corresponde ao ID do colaborador no corpo da solicitação.", Sucesso = false });
+                }
+
+                _context.Entry(colaboradorModel).State = EntityState.Modified;
+
+                await _context.SaveChangesAsync();
+
+                return NoContent();
             }
             catch (DbUpdateConcurrencyException)
             {
-                if (!ColaboradorModelExist(id))
+                if (!ColaboradorModelExist((int)id))
                 {
-                    return NotFound();
+                    return NotFound(new ServiceResponse<ColaboradorModel> { Mensagem = "Colaborador não encontrado.", Sucesso = false });
                 }
                 else
                 {
                     throw;
                 }
             }
-
-            return NoContent();
+            catch (Exception ex)
+            {
+                return StatusCode(500, new ServiceResponse<ColaboradorModel> { Mensagem = $"Ocorreu um erro ao atualizar o colaborador: {ex.Message}", Sucesso = false });
+            }
         }
 
-    }
+        [HttpPatch("{id}")]
+        public async Task<IActionResult> PatchColaboradorModel(int id, JsonPatchDocument<ColaboradorModel> patchDocument)
+        {
+            try
+            {
+                if (patchDocument == null)
+                {
+                    return BadRequest(new ServiceResponse<ColaboradorModel> { Mensagem = "O documento de patch não pode ser nulo.", Sucesso = false });
+                }
 
+                var colaboradorModel = await _context.ColaboradorItems.FindAsync(id);
+
+                if (colaboradorModel == null)
+                {
+                    return NotFound(new ServiceResponse<ColaboradorModel> { Mensagem = "Colaborador não encontrado.", Sucesso = false });
+                }
+
+                patchDocument.ApplyTo(colaboradorModel, ModelState);
+
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(new ServiceResponse<ColaboradorModel> { Mensagem = "As modificações aplicadas não são válidas.", Sucesso = false });
+                }
+
+                _context.Entry(colaboradorModel).State = EntityState.Modified;
+
+                await _context.SaveChangesAsync();
+
+                return NoContent();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ColaboradorModelExist(id))
+                {
+                    return NotFound(new ServiceResponse<ColaboradorModel> { Mensagem = "Colaborador não encontrado.", Sucesso = false });
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new ServiceResponse<ColaboradorModel> { Mensagem = $"Ocorreu um erro ao atualizar o colaborador: {ex.Message}", Sucesso = false });
+            }
+        }
+
+        private bool ColaboradorModelExist(int id)
+        {
+            return _context.ColaboradorItems.Any(e => e.Id == id);
+        }
+    }
 }
--- obj/Debug/net8.0/Api.AssemblyInfo.cs
+++ obj/Debug/net8.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a8948be58eea737e22b3bba8af1c348ad458c38")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+150efb753c65fc4e6555d99db100032b33c2a22c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
